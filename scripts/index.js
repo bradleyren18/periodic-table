31434857e73f5cba642966bb310821a8d9d2e2b3
BradleyRen <148376421+BradleyRen@users.noreply.github.com>
2024-11-04 16:49:22 +0000

    correct styling

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,7 @@ for (let x = 0; x < table.length; x++) {
         ctx.fillRect(xpos, ypos, cellSize, cellSize);
         if (table[x][y] != "black") {
             var text = elements[pos]
-            ctx.font = "20px Courier New";
+            ctx.font = "20px Arial";
             ctx.fillStyle = "black"
 
             const textMetrics = ctx.measureText(text);
@@ -32,14 +32,14 @@ for (let x = 0; x < table.length; x++) {
             ctx.fillText(text, textX, textY);
 
             var text1 = names[pos]
-            ctx.font = "10px Courier New";
+            ctx.font = "10px Arial";
             const textMetrics1 = ctx.measureText(text1);
             const textX1 = xpos + (cellSize - textMetrics1.width) / 2;
             const textY1 = ypos + (cellSize + parseInt(ctx.font, 10)) / 2;
             ctx.fillText(text1, textX1, textY1 + 20);
 
             var text2 = numbers[pos]
-            ctx.font = "15px Courier New";
+            ctx.font = "15px Arial";
             const textMetrics2 = ctx.measureText(text2);
             const textX2 = xpos + (cellSize - textMetrics2.width) / 2;
             const textY2 = ypos + (cellSize + parseInt(ctx.font, 10)) / 2;
